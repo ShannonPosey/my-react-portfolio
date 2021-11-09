1bbf3d1d406593f9097e97e90f72b28635cf381b
shannon_p <snp1284kasey@aol.com>
2021-11-08 19:49:54 -0500

    setting up file structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import './App.css';
+import React from "react";
+import Nav from "./Components/Nav";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+
 
 function App() {
   return (
---
+++ src/Components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+import
+import avataaar from "../img/avataaars.png";
+
+function About() {
+    return (
+
+    )
+}

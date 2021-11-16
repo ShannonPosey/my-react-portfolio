e6298c7f3ec16e7a380e32d19ce2f3a266f108ba
shannon_p <snp1284kasey@aol.com>
2021-11-15 21:37:02 -0500

    adding styling nav

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,14 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 import heroBg from "../img/heroBg.png"
 
 function Hero() {
     return(
         <StyledHero>
-        <div className=" tc bg-light-green">
-            <h1>Shannon Posey</h1>
+        <div className="header">
+            <h1>Shannon Posey | Full Stack Web Developer</h1>
+             <h2>let's <span> code </span> something to cool </h2>
         </div>
+        </StyledHero>
     )
-    </StyledHero>
 };
+
+const StyledHero = styled.section `
+    background-image: url(${heroBg});
+    background-size: cover;
+    
+    .header {
+        height: 700px;
+    }
+
+    h1 {
+        color: #ffffff;
+    }
+    
+    span {
+        color: #fa0075;
+    }
+
+    h2 {
+        clear: both;
+        color: #ffffff;
+        font-size: 72px;
+        padding: 80px 85px 0 85px;
+        width: 1000px;
+        display: flex;
+        justify-content: center;
+        align-item: center;
+
+    }
+`
 
 export default Hero;
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
     return(
         <StyledNav>
         <div className="">
-            <nav>
+            <nav className="nav-link">
                 <ul className="">
                     <li><a href="#about" className="about">About Me</a></li>
                     <li><a href="#projects" className="projects">Projects</a></li>
@@ -18,13 +18,21 @@ function Nav() {
 };
 
 const StyledNav = styled.section `
+background: black;
+.nav-link {
+    border: 2px solid transparent;
+}
+
+.nav-link:hover {   
+    text-decoration: none;
+    border-bottom: 2px solid #fa0075;
+}
     ul, li, a {
         text-decoration: none;
-        padding-right: 25px;
-         display: flex;
+           display: flex;
         justify-content: center;
         align-item: center;
-        color: black;
+        color: #ffffff;
    }
 `
 
